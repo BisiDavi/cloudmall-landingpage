462c22c7d89d0a9ed1bc625aef4fdf71a3f662ea
BisiDavi <oludavidconnect@gmail.com>
2021-03-24 16:09:42 +0100

    homepage

---
+++ components/Button/FilledButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+
+const FilledButton = ({text}) => {
+    return (
+            <Button>{text}</Button>
+    )
+
+export default FilledButton;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,29 +1,50 @@
 import React from "react";
+import Link from "next/link";
 import { Col, Container, Row } from "react-bootstrap";
 import Logo from "../Logo";
 import styles from "../../styles/Header.module.css";
 
 const Header = () => {
-    const menuLinks = ["Get Started", "About Us", "FAQs"];
+    const menuLinks = [
+        { name: "Download CloudMall App", link: "#downloadApp" },
+        { name: "About Us", link: "#about-us" },
+        { name: "FAQs", link: "#faqs" },
+    ];
 
     const displayMenu = () => (
         <ul>
             {menuLinks.map((link) => (
-                <li key={link} className="link">
-                    {link}
+                <li key={link.name} className="link">
+                    <Link href={link.link} passHref>
+                        <a>{link.name}</a>
+                    </Link>
                 </li>
             ))}
             <style jsx>
                 {`
+                    a {
+                        color: black;
+                        text-decoration: none;
+                    }
                     .link {
                         list-style: none;
                         text-decoration: none;
-                        margin: 5px 20px;
+                        margin: 0px 20px;
+                    }
+                    li:first-child {
+                        background-color: orange;
+                        border-radius: 10px;
+                        color: white;
+                        font-weight: bold;
+                        padding: 5px 10px;
+                        cursor: pointer;
+                        margin-top: 0px;
                     }
                     ul {
                         display: flex;
                         justify-content: space-between;
                         width: 25%x;
+                        align-items: center;
                     }
                 `}
             </style>
