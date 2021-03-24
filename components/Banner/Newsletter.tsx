0ee4d61389cbd389f5ee782e6e750886d5e22083
BisiDavi <oludavidconnect@gmail.com>
2021-03-24 20:42:51 +0100

    homepage

--- components/Banner/Newsletter.tsx
+++ components/Banner/Newsletter.tsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { NewsletterForm } from "../Form";
+import styles from "../../styles/Newsletter.module.css";
 
 const Newsletter = () => {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-12">
+        <Container className={styles.newsletter} fluid>
+            <Row>
+                <Col lg={6} xs={12}>
                     <h3>Subscribe to our newsletter today!</h3>
                     <p>
                         Don't miss out on our great offers & Receive deals
                         discount from all our top vendors via e-mail
                     </p>
-                </div>
-                <div className="col-12">
-                    
-                </div>
-            </div>
-        </div>
+                </Col>
+                <Col className={styles.newslettersubscribe} lg={6} xs={12}>
+                    <Row className={styles.overlay}></Row>
+                    <NewsletterForm />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
