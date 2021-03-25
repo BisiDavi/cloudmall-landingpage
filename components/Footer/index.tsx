e0565bf22c1e80d443e0c49d561c6c022c274a62
BisiDavi <oludavidconnect@gmail.com>
2021-03-25 23:32:24 +0100

    homepage

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { AppstoreButton, PlaystoreButton } from "../Button";
 import Logo from "../Logo";
+import { FaFacebook, FaInstagram, FaMedium } from "react-icons/fa";
+import { FiTwitter } from "react-icons/fi";
 
 const Footer = () => {
     const currentDate = new Date();
@@ -12,14 +14,31 @@ const Footer = () => {
             {listArr.map((list) => (
                 <li key={list}>{list}</li>
             ))}
+            <style jsx>
+                {`
+                    ul {
+                        padding: 0px;
+                        margin-top: 20px;
+                    }
+                    li {
+                        list-style: none;
+                        font-weight: bold;
+                        font-size: 20px;
+                    }
+                `}
+            </style>
         </ul>
     );
 
+    // FaFacebook FiTwitter
+
     return (
-        <div className="container">
+        <footer className="container-fluid">
             <div className="row">
                 <div className="col-lg-4">
-                    <Logo />
+                    <span>
+                        <Logo />
+                    </span>
                     <div className="btnGrp">
                         <AppstoreButton />
                         <PlaystoreButton />
@@ -30,7 +49,7 @@ const Footer = () => {
                     {displayList(cities)}
                 </div>
                 <div className="col-lg-4">
-                    <h1>Popular popularVendors</h1>
+                    <h1>Popular vendors</h1>
                     {displayList(popularVendors)}
                 </div>
             </div>
@@ -40,7 +59,34 @@ const Footer = () => {
                 </div>
                 <div className="col-lg-6 col-xs-12 icons"></div>
             </div>
-        </div>
+            <style jsx>{`
+                footer {
+                    background-color: black;
+                    color: white;
+                    width: 100%;
+                    padding: 70px 50px;
+                }
+                .btnGrp {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    width: 300px;
+                    margin: auto;
+                    align-items: center;
+                }
+
+                h1 {
+                    font-size: 25px;
+                }
+                .col-lg-4 {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    margin: auto;
+                }
+            `}</style>
+        </footer>
     );
 };
 
