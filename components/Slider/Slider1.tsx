8ccd2079a252b644d4b18a802f9325fdbed25738
BisiDavi <oludavidconnect@gmail.com>
2021-03-25 13:11:05 +0100

    main

--- components/Slider/Slider1.tsx
+++ components/Slider/Slider1.tsx
@@ -75,68 +75,68 @@ const Slider1 = () => {
                         flex-direction: column;
                     }
                     .item-child.child-1 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 1s;
                     }
                     .item-child.child-2 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 2s;
                     }
                     .item-child.child-3 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 3s;
                     }
                     .item-child.child-4 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 4s;
                     }
                     .item-child.child-5 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 5s;
                     }
                     .item-child.child-6 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 6s;
                     }
                     .item-child.child-7 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 7s;
                     }
                     .item-child.child-8 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 8s;
                     }
                     .item-child.child-9 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 9s;
                     }
                     .item-child.child-10 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 10s;
                     }
                     .item-child.child-11 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 11s;
                     }
                     .item-child.child-12 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 12s;
                     }
                     .item-child.child-13 {
-                        -webkit-animation: 1s ease-in 1 slideup;
-                        animation: 1s ease-in 1 slideup;
+                        -webkit-animation: 1s ease-in alternate slideup;
+                        animation: 1s ease-in alternate slideup;
                         animation-delay: 13s;
                     }
                     .item-child {
