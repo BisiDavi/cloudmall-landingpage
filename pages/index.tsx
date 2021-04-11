d3222989e76e0b48273985265aa50bf591c6f282
BisiDavi <oludavidconnect@gmail.com>
2021-04-11 14:37:54 +0100

    page restructured

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 import Pagelayout from "../containers/Pagelayout";
-import {
-    AlertBanner,
-    InfoBanner,
-    Newsletter,
-    OrderBanner,
-    Policy,
-} from "../components/Banner";
-import { Header } from "../components/Header";
+import { InfoBanner, Newsletter, OrderBanner } from "../components/Banner";
 import { MainSlider } from "../components";
-import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <Pagelayout>
-           
             <OrderBanner />
             <MainSlider />
             <InfoBanner />
             <Newsletter />
-            <Policy />
-            <Footer />
         </Pagelayout>
     );
 }
