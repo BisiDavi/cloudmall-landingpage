bf44ae508ad177948ded7c5277620da8a22459f9
BisiDavi <oludavidconnect@gmail.com>
2021-04-18 12:42:00 +0100

    responsiveness:

--- components/temp/Footer.tsx
+++ components/temp/Footer.tsx
@@ -174,7 +174,16 @@ const Footer = () => {
                             margin: 0px 15px;
                             position: relative;
                         }
+                        footer .stores .client h4 {
+                            font-size: 20px;
+                            margin-bottom: 2px;
+                        }
+                        footer .stores .client p {
+                            font-size: 17px;
+                            margin-bottom: 2px;
+                        }
                     }
+
                     @media (max-width: 480px) {
                         footer {
                             display: flex;
@@ -192,9 +201,98 @@ const Footer = () => {
                             width: 100%;
                             display: grid;
                             grid-template-columns: repeat(2, 1fr);
+                            grid-template-rows: repeat(14, 30px);
+                            grid-gap: 10px;
+                            margin-top: 20px;
                         }
                         .profile {
                             width: 100%;
+                        }
+                        .client.client-0 {
+                            grid-column: 1;
+                            grid-row: 1/9;
+                        }
+                        .client.client-1 {
+                            grid-column: 2;
+                            grid-row: 1/5;
+                        }
+                        .client.client-2 {
+                            grid-row: 5/9;
+                            margin-top: 5px;
+                        }
+                        .client.client-3 {
+                            grid-column: 1;
+                            grid-row: 9/13;
+                            margin-top: 10px;
+                        }
+                        .client.client-4 {
+                            grid-column: 1;
+                            grid-row: 12/14;
+                            margin-top: 5px;
+                        }
+                        .client.client-5 {
+                            grid-row: 10/12;
+                        }
+                        .client.client-6 {
+                            grid-row: 12/14;
+                            margin-top: 10px;
+                        }
+                        .client.client-7 {
+                            grid-column: 2;
+                            margin-top: -10px;
+                            grid-row: 14/16;
+                        }
+                    }
+                    @media (max-width: 380px) {
+                        .client.client-7 {
+                            grid-column: 2;
+                            margin-top: -20px;
+                            grid-row: 15/16;
+                        }
+                        .client.client-6 {
+                            grid-row: 13/15;
+                            grid-column: 2;
+                            margin-top: 0px;
+                        }
+                    }
+                    @media (max-width: 340px) {
+                        .client.client-0 {
+                            grid-column: 1;
+                            grid-row: 1/9;
+                        }
+                        .client.client-1 {
+                            grid-column: 2;
+                            grid-row: 1/5;
+                        }
+                        .client.client-2 {
+                            grid-column: 2;
+                            grid-row: 6/10;
+                            margin-top: -15px;
+                        }
+                        .client.client-3 {
+                            grid-column: 1;
+                            grid-row: 9/13;
+                            margin-top: 10px;
+                        }
+                        .client.client-4 {
+                            grid-column: 1;
+                            grid-row: 12/14;
+                            margin-top: 5px;
+                        }
+                        .client.client-5 {
+                            grid-row: 11/15;
+                            grid-column: 2;
+                            margin-top: -20px;
+                        }
+                        .client.client-6 {
+                            grid-column: 2;
+                            grid-row: 14/16;
+                            margin-top: -10px;
+                        }
+                        .client.client-7 {
+                            grid-column: 1;
+                            grid-row: 14/17;
+                            margin-top: 10px;
                         }
                     }
                 `}
