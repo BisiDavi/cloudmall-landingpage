aa3d540edcabcbf06a3fb8935b154f5a9fadeddf
BisiDavi <oludavidconnect@gmail.com>
2021-05-10 08:22:37 +0100

    button fixed

--- components/Banner/OrderBanner.tsx
+++ components/Banner/OrderBanner.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Col, Container, Row, Button } from "react-bootstrap";
-import { OrderSlider } from "../Slider";
-import styles from "../../styles/Orderbanner.module.css";
-import DownloadApp from "../Button/DownloadApp";
+import React from 'react';
+import { Col, Container, Row, Button } from 'react-bootstrap';
+import { OrderSlider } from '../Slider';
+import styles from '../../styles/Orderbanner.module.css';
+import DownloadApp from '../Button/DownloadApp';
 
 const OrderBanner = () => {
     return (
@@ -10,13 +10,14 @@ const OrderBanner = () => {
             <Row>
                 <Col className={styles.column1} lg={7} sm={12}>
                     <h1>
-                        We bring your <span>favourite local stores </span> to your finger tips.
+                        We bring your <span>favourite local stores </span> to
+                        your finger tips.
                     </h1>
                     <p>
                         Order for your friends, colleagues and get it delivered
                         quickly
                     </p>
-                    <DownloadApp />
+                    {/* <DownloadApp /> */}
                 </Col>
                 <Col className={styles.column2} lg={5}>
                     <OrderSlider />
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import Logo from "../Logo";
-import styles from "../../styles/Header.module.css";
-import DownloadApp from "../Button/DownloadApp";
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Logo from '../Logo';
+import styles from '../../styles/Header.module.css';
+import DownloadApp from '../Button/DownloadApp';
 
 const Header = () => {
     return (
         <Container className={styles.header} fluid>
             <Row>
-                <Col className="d-flex justify-content-between" lg={12} sm={12}>
+                <Col className='d-flex justify-content-between' lg={12} sm={12}>
                     <Container
-                        as="nav"
+                        as='nav'
                         className={`px-lg-5 px-0 ${styles.nav}`}
                         fluid
                     >
                         <Logo />
-                        <DownloadApp />
+                        {/* <DownloadApp /> */}
                     </Container>
                 </Col>
             </Row>
