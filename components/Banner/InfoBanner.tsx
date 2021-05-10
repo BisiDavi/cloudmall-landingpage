fe11f999f8e5703b7ecb1e0bb486fa645706ed0d
BisiDavi <oludavidconnect@gmail.com>
2021-05-10 09:43:50 +0100

    main

--- components/Banner/InfoBanner.tsx
+++ components/Banner/InfoBanner.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Image from "next/image";
-import styles from "../../styles/InfoBanner.module.css";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Image from 'next/image';
+import styles from '../../styles/InfoBanner.module.css';
 
 const InfoBanner = () => {
     const procedureArr = [
         {
-            icon: "/delivery.svg",
-            title: "Set your delivery address",
-            text: "Choose your present location, for delivery purpose.",
+            icon: '/delivery.svg',
+            title: 'Set your delivery address',
+            text: 'Choose your present location, for delivery purpose.',
         },
         {
-            icon: "/product.svg",
-            title: "Order the Product",
-            text: "Browse stores for your desired products and order",
+            icon: '/product.svg',
+            title: 'Order the Product',
+            text: 'Browse stores for your desired products and order',
         },
         {
-            icon: "/bike.svg",
-            title: "Doorstep Delivery",
-            text: "Get your order",
+            icon: '/bike.png',
+            title: 'Doorstep Delivery',
+            text: 'Get your order',
         },
     ];
     return (
         <Container className={`p-lg-5 p-3 ${styles.banner}`} fluid>
             <Row>
                 <Col lg={12} xs={12}>
-                    <h1 className="text-center">How it Works</h1>
+                    <h1 className='text-center'>How it Works</h1>
                 </Col>
                 {procedureArr.map((procedure, index) => (
                     <Col
